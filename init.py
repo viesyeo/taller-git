3658b9e54aff349ccf9ba81550aba536cdd2c4a7
viesyeo <co961104@gmail.com>
2023-10-30 23:10:38 -0500

    elevando

--- init.py
+++ init.py
@@ -1,9 +1,9 @@
-from operaciones import divi
+from operaciones import poten
 def game():
     score = 0
     while True:
         print('========Menu========'
-              '\n1.división'
+              '\n1.potenciación'
               '\n0.Exit')
         option = int(input('\nChoice an option:')) 
         if option == 0:
@@ -13,9 +13,9 @@ def game():
         answer=int(input('Enteryouanswer:'))
 
         if option==1: 
-            result = num_1 / num_2
+            result = num_1 ** num_2
             if result == answer:
-                score+=2
+                score+=4
                 print('Correct!!')
             else:
                 print('Incorrect')
--- operaciones.py
+++ operaciones.py
@@ -1,4 +1,4 @@
-def divi(num_1,num_2):
-    result = num_1 / num_2
-    print(f'{num_1} / {num_2} is equal to {result}') 
+def poten(num_1,num_2):
+    result = num_1 ** num_2
+    print(f'{num_1} elevado a {num_2} is equal to {result}') 
     return result
