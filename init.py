f77ba398f2a4ae6f3aaa82bce65b75e842cc3593
anadan10102006 <141078706+anadan10102006@users.noreply.github.com>
2023-10-30 21:47:00 -0500

    Add files via upload

---
+++ init.py
@@ -0,0 +1,26 @@
+from operations import add
+def game():
+    score = 0
+    while True:
+        print('========Menu========'
+              '\n1.Add'
+              '\n0.Exit')
+        option = int(input('\nChoice an option:')) 
+        if option == 0:
+            break
+        num_1=input('Enterfirstnumber:')
+        num_2=input('Entersecondnumber:')
+        answer=int(input('Enteryouanswer:'))
+
+        if option==1: 
+            result=add(num_1,num_2)
+            if result == answer:
+                score+=1
+                print('Correct!!')
+            else:
+                print('Incorrect')
+
+            print(f'========GameOver========'
+            f'\nYouscoreis{score}'
+            '\nKeepgoing!')
+            game()
